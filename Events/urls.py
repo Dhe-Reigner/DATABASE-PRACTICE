9084857ccc69807724400111903f8707e774e422
Dhe-Reigner <martinkagema01@gmail.com>
2024-11-24 06:47:47 +0300

    Initial commit

--- Events/urls.py
+++ Events/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
    path('', views.home, name='home'),
+   path('participants',views.participants, name='participants'),
 ]
--- Events/views.py
+++ Events/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from . models import Event
+from . models import Participant 
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    all_events =Event.objects.all()
+    return render(request, 'home.html', {
+        'merry':all_events
+    })
+
+def participants(request):
+    all_participants = Participant.objects.all()
+    return render(request, 'participants.html',{
+        'attendants':all_participants
+    })
